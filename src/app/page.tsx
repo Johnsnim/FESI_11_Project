f116906cd27f9ce2e48d9657d45bee3b2aa59e91
hwangseunghyun <hwamg123@naver.com>
2025-09-19 16:11:15 +0900

    tailwind design 색상추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      반갑습니다 세준님 다연님
+    <div className="bg-gradient-100 flex size-full flex-col items-center justify-center">
+      <p className="font-tenada text-3xl font-extrabold text-green-800">
+        Tenada
+      </p>
+      <p className="text-6xl font-bold text-blue-400">세준님</p>
+      <p className="text-6xl font-bold text-green-500">다연님</p>
     </div>
   );
 }
--- src/shared/services/auth/auth.service.ts
+++ src/shared/services/auth/auth.service.ts
@@ -1,17 +1,18 @@
-import { api } from "@/lib/api/api"
-import { useAuthStore } from "@/shared/store/auth.store"
+import { api } from "@/lib/api/api";
+import { useAuthStore } from "@/shared/store/auth.store";
+
+const TEAM_ID = process.env.NEXT_PUBLIC_TEAM_ID;
 
 export class AuthService {
   async login(email: string, password: string): Promise<{ token: string }> {
     const { data } = await api.post<{ token: string }>(
-      "/team4/auths/signin",
+      `/${TEAM_ID}/auths/signin`,
       { email, password },
-    )
-    // Zustand + localStorage에 저장
-    useAuthStore.getState().setToken(data.token)
-    localStorage.setItem("accessToken", data.token)
-    return data
+    );
+    useAuthStore.getState().setToken(data.token);
+    localStorage.setItem("accessToken", data.token);
+    return data;
   }
 }
 
-export const authService = new AuthService()
+export const authService = new AuthService();
