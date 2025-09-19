04574343bbbd87650f3a389c67ca526a51af332a
qw506799 <qw506799@naver.com>
2025-09-19 17:31:54 +0900

    공용 컴포넌트 테스트

---
+++ src/app/(public)/list/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { AppButton } from "@/shared/components/button";
+
+export default function ListPage() {
+  return (
+    <>
+      <AppButton>참여하기</AppButton>
+    </>
+  );
+}
