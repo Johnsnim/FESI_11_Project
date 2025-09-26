67d1daf7d16dc4056dcd2d16d06032c09d5e3bd4
hwangshyun <hwamg123@naver.com>
2025-09-26 13:23:13 +0900

    fix : auth-banner 넥스트 이미지 컴포넌트로 변경

--- src/shared/components/ auth-banner.tsx
+++ src/shared/components/ auth-banner.tsx
@@ -1,15 +1,23 @@
+import Image from "next/image";
+
 export default function AuthBanner() {
   return (
     <>
-      <img
+      <Image
         src="/image/img_login_sm.svg"
         alt="로그인이미지(작은 화면)"
-        className="block w-[272px] md:hidden"
+        width={0}
+        height={0}
+        unoptimized
+        className="block w-[272px] md:hidden h-auto"
       />
-      <img
+      <Image
         src="/image/img_login_lg.svg"
         alt="로그인이미지(큰 화면)"
-        className="hidden w-[451px] md:block lg:w-[533px]"
+        width={0}
+        height={0}
+        unoptimized
+        className="hidden w-[451px] md:block lg:w-[533px] h-auto"
       />
     </>
   );
