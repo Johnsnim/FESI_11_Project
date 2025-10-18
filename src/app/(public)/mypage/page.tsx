835988c8b50f19532d7c3815438d95cfd24c9bb1
hwangshyun <hwamg123@naver.com>
2025-10-18 16:55:55 +0900

    feat : ui세부 조정 , JoinedGatherings컴포넌트에 기능 연결

--- src/app/(public)/mypage/page.tsx
+++ src/app/(public)/mypage/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Info from "@/features/mypage/components/info";
+import JoinedGatherings from "@/features/mypage/components/Joined-Gatherings";
 import MyPageTabs, { TabItem } from "@/features/mypage/components/mypage-tabs";
 import UserEditModal from "@/features/mypage/components/user-edit-modal";
 import {
@@ -11,19 +12,27 @@ import {
   useUpdateUserMutation,
   useUserQuery,
 } from "@/shared/services/auth/use-auth-queries";
+import {
+  useJoinedGatheringsQuery,
+  useLeaveGatheringMutation,
+} from "@/shared/services/gathering/use-gathering-queries";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useState } from "react";
 import { useForm } from "react-hook-form";
 
-// useSearchParams를 사용하는 부분을 별도 컴포넌트로 분리
 function MyPageContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const { data: user, isLoading } = useUserQuery();
   const updateUser = useUpdateUserMutation();
   const [modalOpen, setModalOpen] = useState(false);
+  const leaveGathering = useLeaveGatheringMutation();
 
+  const { data: joinedGatherings, isLoading: isJoinedLoading } =
+    useJoinedGatheringsQuery();
+
+  //훅폼 , 모달 훅
   const form = useForm<EditUserFormValues>({
     resolver: zodResolver(EditUserSchema),
     mode: "onChange",
@@ -46,6 +55,14 @@ function MyPageContent() {
     });
   };
 
+  //모임 참여취소 훅
+  const handleCancelGathering = (id: number) => {
+    if (confirm("정말 이 모임 참여를 취소하시겠습니까?")) {
+      leaveGathering.mutate(id);
+    }
+  };
+
+  //탭 훅
   const currentTab = searchParams.get("tab") ?? "meetings";
 
   const handleTabChange = (tab: string) => {
@@ -53,15 +70,16 @@ function MyPageContent() {
   };
 
   const tabs: TabItem[] = [
-    { value: "meetings", label: "나의 모임" },
+    { value: "joinedgatherings", label: "나의 모임" },
     { value: "reviews", label: "나의 리뷰" },
     { value: "created", label: "내가 만든 모임" },
   ];
+  ///여기까지
 
   if (isLoading) return <div className="p-4">로딩중...</div>;
 
   return (
-    <div className="flex w-full flex-col px-4 md:px-6 lg:flex-row">
+    <div className="lg-gap-10 flex w-full flex-col gap-6 px-4 md:px-6 lg:flex-row">
       {user && (
         <Info
           user={user}
@@ -70,15 +88,21 @@ function MyPageContent() {
           }}
         />
       )}
-      <div className="w-full">
+      <div className="mb-10 w-full">
         <MyPageTabs
           currentTab={currentTab}
           tabs={tabs}
           onChange={handleTabChange}
         />
 
-        <div className="mt-6">
-          {currentTab === "meetings" && <div />}
+        <div className="mt-6 lg:mt-8">
+          {currentTab === "joinedgatherings" && (
+            <JoinedGatherings
+              data={joinedGatherings}
+              isLoading={isJoinedLoading}
+              onCancel={handleCancelGathering}
+            />
+          )}
           {currentTab === "reviews" && <div />}
           {currentTab === "created" && <div />}
         </div>
